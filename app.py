92021821ec2a08abb20e6b2b53da9c5cb7f75222
KuganParthiban <kugan.parthiban@gmail.com>
2023-05-25 11:15:29 +0800

    updated app.py

--- app.py
+++ app.py
@@ -1,49 +1,14 @@
 import streamlit as st
 import numpy as np
-import pandas as pd
-from sklearn import datasets
-from sklearn.ensemble import RandomForestClassifier
 
-st.write("""
-# Simple Iris Flower Prediction App
+st.title("Create a Selectbox")
 
-This app predicts **Iris Flower** Type!      
-""")
+contact_options = ["Emai'", "Phone", "WhatsApp"]
 
-st.sidebar.header('User Input Parameters')
+st.header("Selectbox from a list")
 
-def user_input_features():
-    sepal_length = st.sidebar.slider('Sepal length', 4.3, 7.9, 5.4)
-    sepal_width = st.sidebar.slider('Sepal width', 2.0, 4.4, 3.4)
-    petal_length = st.sidebar.slider('Petal length', 1.0, 6.9, 1.3)
-    petal_width = st.sidebar.slider('Petal width', 0.1, 2.5, 0.2)
-    data = {'sepal_length': sepal_length,
-            'sepal_width': sepal_width,
-            'petal_length': petal_length,
-            'petal_width': petal_width}
-    features = pd.DataFrame(data, index=[0])
-    return features
+contact_selected = st.selectbox("How would you like to be contacted ?",
+                                options=contact_options)
 
-df = user_input_features()
-
-st.subheader('User Input Parameters')
-st.write(df)
-
-iris = datasets.load_iris()
-X = iris.data
-Y = iris.target
-
-clf = RandomForestClassifier()
-clf.fit(X,Y)
-
-prediction = clf.predict(df)
-prediction_proba = clf.predict_proba(df)
-
-st.subheader('Class labels and their corresponding indec number')
-st.write(iris.target_names)
-
-st.subheader('Prediction')
-st.write(iris.target_names[prediction])
-
-st.subheader('Prediction Probability')
-st.write(prediction_proba)
+st.write("Selectbox returns:", contact_selected,
+         "of type", type(contact_selected))
